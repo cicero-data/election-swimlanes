b562c9efd755f27e1fc1e6fcd7d6b892aa23b1f6
Luke McKinstry <lukemckinstry@gmail.com>
2020-06-11 10:41:57 -0400

    Updates

---
+++ precache-manifest.97096026657ee2eb06ad180cc64eb63b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f80368702e608a38bfe262a735a44b",
+    "url": "/election-swimlanes/index.html"
+  },
+  {
+    "revision": "4f6f899d8acf6f7ad42c",
+    "url": "/election-swimlanes/static/css/main.b66a3d18.chunk.css"
+  },
+  {
+    "revision": "654f9f7598fa48708dc8",
+    "url": "/election-swimlanes/static/js/2.4ed39d64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/election-swimlanes/static/js/2.4ed39d64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f6f899d8acf6f7ad42c",
+    "url": "/election-swimlanes/static/js/main.432033a7.chunk.js"
+  },
+  {
+    "revision": "3490d59d03002e3e2299",
+    "url": "/election-swimlanes/static/js/runtime-main.55f578f1.js"
+  }
+]);
